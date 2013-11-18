817129865a35db8663c3b85371ea75e266baca9f
Gerald Bauer <gerald.bauer@gmail.com>
2013-11-18 23:50:43 +0100

    polish check style param

--- planet.rb
+++ planet.rb
@@ -53,57 +53,78 @@ class Planet < Sinatra::Base
     404  # return 404 not found - sorry - no favicon.ico - we use favicon.png
   end
 
-
-  get '/' do
-    # add ?style='random' - to make it clear style is random for user
-    #  plus add key of first planet
-    key = Site.first.key
-    style = params[:style] || 'random'   # default style is random
-    redirect "#{path_prefix}/#{key}?style=#{style}"
+=begin
+  # todo - use double routes w/ next and redirect - easier? cleaner? why? why not??
+  get '/:key?' do
+    if params[:style]
+      next
+    else
+      redirect "#{path_prefix}/ # -- request_uri ?? plus add style query param -- how?? 
+    end
   end
+=end
 
 
   get '/:key?' do
-    key   = params[:key]
 
-    puts "  get /:key?  |  key: >#{key}<"
+    key   = params[:key] 
+    style = params[:style]
 
-    key = Site.first.key    if key.nil?
+    puts "  get /:key?  |  key: >#{key}<, style: >#{style}<"
 
-    ### todo/fix: if no style present - how can i (best) redirect to add ?style=
-    # to browser url?? - use sinatra constraints??? check it
-    # to avoid confusion - always show/include ?style=<theme>
+    # make sure style params always gets shown in browser address box
+    #  -- helps w/ bookmarking and hopefully avoids confusion w/ users
+    # -- add ?style='random' - to make it clear style is random for user
+    if style.nil?
+ 
+      ## todo/fix: use redirect "#{path_prefix}+ #{request_uri} ?? plus add style query param -
+      ##   -easier/cleaner (make it on line)-  how?? - why? why not??
 
-    style = params[:style] || 'random'   # default style is random
+      if key.nil?
+        redirect "#{path_prefix}/?style=random"  # default style is random
+      else
+        redirect "#{path_prefix}/#{key}?style=random"  # default style is random        
+      end
+    else
 
-    if ['random', 'rnd', 'r', 'lucky', 'luck', 'shuffle' ].include?( style )
-      # random style - throw the dice for surprise
-      # note: zero based e.g. 0,1,2,3,4,5 for array inden
-      style = ['std','cards','news','top','hacker','digest'][ rand(6) ]
+      if key
+        site = Site.find_by_key!( key )
+      else
+        # note: use order(:id) to make sure site pos(ition) will not get changed by db updates
+        site = Site.order(:id).first
+        key  = site.key
+      end
+
+      if ['random', 'rnd', 'r', 'lucky', 'luck', 'shuffle' ].include?( style )
+        # random style - throw the dice for surprise
+        # note: zero based e.g. 0,1,2,3,4,5 for array inden
+        style_lottery = ['std','cards','news','top','hacker','digest']
+        style = style_lottery[ rand( style_lottery.size ) ]
+      end
+
+      if ['cards','c','ii','2'].include?( style )
+        tpl    = :'blank.cards'
+        layout = :'blank.cards_layout'
+      elsif ['news','n','iii','3'].include?( style )
+        tpl    = :'news'
+        layout = :'news_layout'
+      elsif ['top','t','iv','4'].include?( style )
+        tpl    = :'top'
+        layout = :'top_layout'
+      elsif ['hacker','h','v','5'].include?( style )
+        tpl    = :'hacker'
+        layout = :'hacker_layout'    
+      elsif ['digest','d','vi','6'].include?( style )
+        tpl    = :'digest'
+        layout = :'digest_layout'
+      else   ### if ['blank','std','b','1','i'].include?( style )
+        tpl    = :'blank'
+        layout = :'blank_layout'
+      end
+      
+      erb tpl, :layout => layout, locals: { site: site }
     end
-
-    if ['cards','c','ii','2'].include?( style )
-      tpl    = :'blank.cards'
-      layout = :'blank.cards_layout'
-    elsif ['news','n','iii','3'].include?( style )
-      tpl    = :'news'
-      layout = :'news_layout'
-    elsif ['top','t','iv','4'].include?( style )
-      tpl    = :'top'
-      layout = :'top_layout'
-    elsif ['hacker','h','v','5'].include?( style )
-      tpl    = :'hacker'
-      layout = :'hacker_layout'    
-    elsif ['digest','d','vi','6'].include?( style )
-      tpl    = :'digest'
-      layout = :'digest_layout'
-    else   ### if ['blank','std','b','1','i'].include?( style )
-      tpl    = :'blank'
-      layout = :'blank_layout'
-    end
-
-    erb tpl, :layout => layout, locals: { site: Site.find_by_key!( key ) }
-  end
+  end # get '/:key?'
 
 
 end # class Planet
