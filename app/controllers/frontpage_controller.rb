6f45df6bd58961dc65ad6558a06cc3c2d00b81bf
Gerald Bauer <gerald.bauer@gmail.com>
2015-01-25 14:07:20 +0100

    add frontpage

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -3,7 +3,28 @@
 class FrontpageController < ApplicationController
 
   def index
+    ## Note: add mark string as safe html (use #html_safe)
+    render :html => FrontpageRenderer.new.render.html_safe
   end
 
 end  # class FrontpageController
 
+
+class FrontpageRenderer
+  #######################
+  # Models
+
+  include Pluto::Models   # e.g. Feed, Item, Site, etc.
+
+  ###################
+  # Helpers
+
+  include TextUtils::HypertextHelper
+  include TextUtils::DateHelper
+
+  def render
+    tmpl = File.read( "#{Rails.root}/app/views/frontpage/index.html.erb" )
+    ERB.new(tmpl).result(binding)
+  end
+
+end # class TimelineRenderer
